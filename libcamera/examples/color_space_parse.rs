12d98d09f58e3a0b5622bfc437009e7e29c784f6
Mathias <70431366+SoZ0@users.noreply.github.com>
2025-11-29 00:17:19 -0800

    colour space fixes

--- libcamera/examples/color_space_parse.rs
+++ libcamera/examples/color_space_parse.rs
@@ -4,10 +4,11 @@ use std::str::FromStr;
 use libcamera::{color_space::ColorSpace, pixel_format::PixelFormat};
 
 fn main() {
-    let cs_str = "Smpte170m/Rec709/Full";
+    let cs = ColorSpace::rec709();
+    let cs_str = cs.to_repr();
     let pf_str = "YUYV";
 
-    let mut cs = ColorSpace::from_string(cs_str).expect("parse color space");
+    let mut cs = ColorSpace::from_string(&cs_str).expect("parse color space");
     let pf = PixelFormat::from_str(pf_str).expect("parse pixel format");
 
     println!("Parsed color space: {}", cs);
